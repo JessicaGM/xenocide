3d206475e249070f27475062b7b27a0cce1ea5bf
Jessica <60440955+JessicaGM@users.noreply.github.com>
2021-11-03 14:51:42 +0000

    initial release

---
+++ resources/views/index.blade.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{secure_asset('css/normalize.css')}}">
+    <link rel="stylesheet" href="{{secure_asset('css/app.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
+        integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script src="{{asset('js/script.js')}}" defer></script>
+    <title>Xenocide</title>
+</head>
+
+<body>
+    <!-- Start navbar -->
+    <nav class="navbar">
+        <div class="logo">Team 5</div>
+        <a href="#" class="toggle-btn">
+            <span class="line"></span>
+            <span class="line"></span>
+            <span class="line"></span>
+        </a>
+        <div class="nav-links">
+            <ul>
+                <li><a href="#game">Game</a></li>
+                <li><a href="#about">About us</a></li>
+                <li><a href="#team">Our team</a></li>
+            </ul>
+        </div>
+    </nav>
+    <!-- End navbar -->
+
+    <!-- Start hero section -->
+    <section id="hero" class="hero container">
+        <div class="hero-content">
+            <div class="texture"></div>
+            <video src="{{secure_asset('images/HexBg.mp4')}}" muted loop autoplay>Your browser does not support the
+                video</video>
+            <h1 class="hero-heading">Xenocide</h1>
+            <h2 class="hero-subheading">Coming soon...</h2>
+            <a href="#game" type="button" class="hero-cta">&#x2193</a>
+        </div>
+    </section>
+    <!-- End hero section -->
+
+    <!-- Start game section -->
+    <section class="game container" id="game">
+        <div class="game-content">
+            <h1 class="game-heading">Game</h1>
+            <p class="game-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel felis iaculis,
+                mollis lacus at, mollis sapien. Nam dictum sodales enim, quis placerat augue varius in. Donec elementum
+                nibh a magna consectetur aliquet. Vivamus fermentum tincidunt leo, ac elementum elit eleifend ac. Sed
+                erat neque, ultrices quis cursus at, pulvinar vitae orci.</p>
+        </div>
+        <div class="game-img">
+            <div class="game-img-wrapper">
+                <img src="{{secure_asset('images/sampleimage.jpg')}}" alt="preview of the game">
+            </div>
+        </div>
+    </section>
+    <!-- End game section -->
+
+    <!-- Start about us section -->
+    <section class="about container" id="about">
+        <div class="about-content">
+            <h1 class="about-heading">About us</h1>
+            <p class="about-paragraph">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel felis iaculis,
+                mollis lacus at, mollis sapien. Nam dictum sodales enim, quis placerat augue varius in. Donec elementum
+                nibh a magna consectetur aliquet. Vivamus fermentum tincidunt leo, ac elementum elit eleifend ac. Sed
+                erat neque, ultrices quis cursus at, pulvinar vitae orci.</p>
+        </div>
+    </section>
+    <!-- End about us section -->
+
+    <!-- Start our team section -->
+    <section class="team container" id="team">
+        <div class="team-content">
+            <h1 class="team-heading">Meet our team</h1>
+            <div class="card-container">
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-img">
+                            <img src="{{secure_asset('images/img.jpg')}}" alt="">
+                        </div>
+                        <div class="card-info">
+                            <h4>Name</h4>
+                            <h5>Role</h5>
+                        </div>
+                        <div class="card-socials">
+                            <a href=""><i class="fab fa-github"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-img">
+                            <img src="{{secure_asset('images/img.jpg')}}" alt="">
+                        </div>
+                        <div class="card-info">
+                            <h4>Name</h4>
+                            <h5>Role</h5>
+                        </div>
+                        <div class="card-socials">
+                            <a href=""><i class="fab fa-github"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-img">
+                            <img src="{{secure_asset('images/img.jpg')}}" alt="">
+                        </div>
+                        <div class="card-info">
+                            <h4>Name</h4>
+                            <h5>Role</h5>
+                        </div>
+                        <div class="card-socials">
+                            <a href=""><i class="fab fa-github"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-img">
+                            <img src="{{secure_asset('images/img.jpg')}}" alt="">
+                        </div>
+                        <div class="card-info">
+                            <h4>Name</h4>
+                            <h5>Role</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-img">
+                            <img src="{{secure_asset('images/img.jpg')}}" alt="">
+                        </div>
+                        <div class="card-info">
+                            <h4>Name</h4>
+                            <h5>Role</h5>
+                        </div>
+                        <div class="card-socials">
+                            <a href=""><i class="fab fa-github"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-img">
+                            <img src="{{secure_asset('images/img.jpg')}}" alt="">
+                        </div>
+                        <div class="card-info">
+                            <h4>Name</h4>
+                            <h5>Role</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-img">
+                            <img src="{{secure_asset('images/img.jpg')}}" alt="">
+                        </div>
+                        <div class="card-info">
+                            <h4>Name</h4>
+                            <h5>Role</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-img">
+                            <img src="{{secure_asset('images/img.jpg')}}" alt="">
+                        </div>
+                        <div class="card-info">
+                            <h4>Name</h4>
+                            <h5>Role</h5>
+                        </div>
+                    </div>
+                </div>
+                <div class="card">
+                    <div class="card-content">
+                        <div class="card-img">
+                            <img src="{{secure_asset('images/img.jpg')}}" alt="">
+                        </div>
+                        <div class="card-info">
+                            <h4>Name</h4>
+                            <h5>Role</h5>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Start footer section -->
+    <footer class="footer">
+        <div class="footer-container">
+            <div class="footer-left">
+                <h4 class="footer-heading">&copy 2021 Team 5 Xenocide</h4>
+            </div>
+            <div class="footer-right">
+                <h4 class="footer-contact">Contact us: team5xenocide@outlook.com</h4>
+            </div>
+        </div>
+    </footer>
+    <!-- End footer section -->
+
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,5 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get(uri:'/', action:'App\Http\Controllers\PagesController@index');
