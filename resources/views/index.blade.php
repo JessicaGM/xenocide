0f477c5bf59363b5f888fd6c2e0003b56853369c
Jessica <60440955+JessicaGM@users.noreply.github.com>
2021-11-30 21:03:12 +0000

    reusable footer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -229,16 +229,7 @@
     <!-- End our team section -->
 
     <!-- Start footer section -->
-    <footer class="footer">
-        <div class="footer-container">
-            <div class="footer-left">
-                <h4 class="footer-heading">&copy {{ date('Y') }} Team 5 Xenocide</h4>
-            </div>
-            <div class="footer-right">
-                <h4 class="footer-contact">Contact us: team5xenocide@outlook.com</h4>
-            </div>
-        </div>
-    </footer>
+    @include('layouts.footer')
     <!-- End footer section -->
 
 </body>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="footer-container">
+        <div class="footer-left">
+            <h4 class="footer-heading">&copy {{ date('Y') }} Team 5 Xenocide</h4>
+        </div>
+        <div class="footer-right">
+            <h4 class="footer-contact">Contact us: team5xenocide@outlook.com</h4>
+        </div>
+    </div>
+</footer>
